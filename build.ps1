667e44581e518fb5d6ad678d72f138bddbe19846
idoja <idojakob@gmail.com>
2024-02-27 18:59:21 +0200

    Update `build.ps1` With Initial Overwrite Of `appsettings.json`

--- build.ps1
+++ build.ps1
@@ -19,12 +19,37 @@ param (
 )
 
 
-# Rewrite appsettings.json with the given parameter.
+### Rewrite appsettings.json with the given parameter.
+
+cd EnvPOC1
+
+# Read appsettings.json to `$appsettings`.
+$appsettings = Get-Content -Path EnvPOC\appsettings.json -Raw | ConvertFrom-Json
+
+# Deep copy `$appsettings` to `$appsettingsTemp`.
+$appsettingsTemp = $appsettings | ConvertTo-Json | ConvertFrom-Json
+
+# Set the Environemnt key of `$appsettings` the given `$Env` parameter.
+$appsettings.Environment = $Env
+
+Write-Output $appsettingsTemp
+Write-Output $appsettings
+
+# Overwrite `$appsettings` to appsettings.json.
+$appsettings | ConvertTo-Json | Set-Content -Path EnvPOC\appsettings.json
 
 # Build EnvPOC1
-cd EnvPOC1
 dotnet build
+
+# Restore appsettings.json to its original state
+$appsettingsTemp | ConvertTo-Json | Set-Content -Path EnvPOC\appsettings.json
+
 cd ..
+
+
+
+
+
 
 # Build EnvPOC2
 cd EnvPOC2
