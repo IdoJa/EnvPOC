d4681a4be99aeadd21690a4ab04c833bc3999d3f
idoja <idojakob@gmail.com>
2024-03-09 12:38:29 +0200

    Add Support For Dotnet 5 In `EnvironmentSetter.cs`

--- EnvPOC1/EnvPOC/EnvironmentSetter.cs
+++ EnvPOC1/EnvPOC/EnvironmentSetter.cs
@@ -20,6 +20,21 @@ namespace EnvPOC
             }
         }
 
+        /// <summary>
+        /// Creates a configuration root based on the specified environment in
+        /// the appsettings.json, mostly used in dotnet 5.
+        /// </summary>
+        public static IConfigurationRoot CreateConfigurationRoot()
+        {
+            return new ConfigurationBuilder()
+                    .AddJsonFile($"appsettings.{_env}.json", false)
+                    .Build();
+        }
+
+        /// <summary>
+        /// Creates a builder based on the specified environment in the
+        /// appsettings.json, mostly used in dotnet 6 and above.
+        /// </summary>
         public static WebApplicationBuilder CreateWebApplicationBuilder(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
